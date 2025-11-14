5fb7106e6157e42633187c6e3b092ac8350219e1
Nirajan chaudhary <nirajanchaudhary00001@gmail.com>
2025-11-14 14:31:16 +0545

    feat: siuuuu

--- backend/src/controller/signupcontroller.js
+++ backend/src/controller/signupcontroller.js
@@ -23,7 +23,7 @@ export const Register = async (req, res, next) => {
     };
 
     let expiryInfo = {
-      expiresIn: "1h",
+      expiresIn: "5h",
     };
     let token = await jwt.sign(infoObj, secretKey, expiryInfo);
 
@@ -34,6 +34,7 @@ export const Register = async (req, res, next) => {
       <h1>Account Registered </h1>
       <p>Your account has been registered. Click below to verify:</p>
 <a href="http://localhost:3000/verify-email?token=${token}">Verify Email</a>
+<p>${token}</p>
 
       `,
     });
@@ -73,3 +74,75 @@ export const verifyEmail = async (req, res, next) => {
     data : user,
   })
 };
+
+
+
+export const login = async (req , res, next)=>{
+  try {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    let user = await Signup.findOne({email : email})
+    if(!user){
+      throw new Error("Invalid Credentials");
+    }
+    if(!user.isVerifiedEmail){
+      throw new Error("Invalid Credentials");
+    }
+    let isValidPassword = await bcrypt.compare(password , user.password)
+    if(!isValidPassword){
+      throw new Error("Invalid Credentials")
+    }
+
+     let infoObj = {
+      _id: user._id,
+    };
+    let expiryInfo = {
+      expiresIn: "365d",
+    };
+    let token = await jwt.sign(infoObj, secretKey, expiryInfo);
+    res.status(200).json({
+      success: true,
+      message: "Signup successfully",
+      user: user,
+      token: token,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success : false,
+      message : error.message
+    })
+  }
+}
+
+export const updatePassword = async(req, res , next)=>{
+  try {
+    let id = req._id;
+    let oldPassword = req.body.oldPassword;
+    let newPassword = req.body.newPassword;
+    let user = await Signup.findById(id);
+    let isValidPassword = await bcrypt.compare(oldPassword , user.password);
+    if(isValidPassword){
+      let hashedPassword = await bcrypt.hash(newPassword , 10);
+      let result = await Signup.findByIdAndUpdate(
+        id,
+        {password : hashedPassword},
+        {new : true}
+
+      )
+    
+      
+      res.status(200).json({
+        success : true,
+        message : "Password updated successfully",
+        result : result
+      })
+    }
+
+  } catch (error) {
+    res.status(400).json({
+      success : false,
+      message : error.message
+    })
+  }
+}
--- backend/src/routes/signupRouter.js
+++ backend/src/routes/signupRouter.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { Register } from "../controller/signupcontroller.js";
+import { login, Register, updatePassword, verifyEmail } from "../controller/signupcontroller.js";
+import { isAuthenticated } from "../middleware/isAuthenticated.js";
 
 export const signupRouter = Router();
 signupRouter.post("/register", Register);
+signupRouter.post("/verify-email",verifyEmail)
+signupRouter.post("/login",login)
+signupRouter.patch("/updatePassword",isAuthenticated,updatePassword)
